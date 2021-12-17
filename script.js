29b376868a72187d2b940f4bf598d32a0a7ac5a7
Matthew <26681440+mattyd96@users.noreply.github.com>
2021-12-17 22:36:06 +1030

    added comments and extended special character options for password

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
@@ -16,61 +15,29 @@ generateBtn.addEventListener("click", writePassword);
 
 //Generate Password
 function generatePassword() {
-  //get user prefferences for password structure
 
-  //used to verify input has been validated
-  // let isValidated = false;
-
-  // //get password length from user between 8 and 128 characters
-  // let length = 8;
-
-  // while(!isValidated) {
-  //   length = parseInt(prompt("Please choose a password length between 8 and 128 characters", "8"));
-  //   if(length >= 8 && length <= 128) {isValidated = true}
-  // }
-  
-  // isValidated = false;
-  // let lower,upper,numeric,special;
-
-  // while(!isValidated) {
-  //   //include lowercase ?
-  //   lower = confirm("Include lowercase letters?");
-
-  //   //include uppercase ?
-  //   upper = confirm("Include uppercase letters?");
-
-  //   //include numeric character?
-  //   numeric = confirm("Include numbers?");
-
-  //   //inlcude special characters
-  //   special = confirm("Include special characters? (!,@,# etc.)");
-
-  //   if(lower || upper || numeric || special) {
-  //     isValidated = true;
-  //   } else {
-  //     alert("please select at least one option");
-  //   }
-  // }
-
-  //let length, lower, upper, numeric, special;
-
-  let [length, lower, upper, numeric, special] = validate();
-
+  //strings holding character sets for each option
   const lowerString = "abcdefghijklmnopqrstuvwxyz";
   const upperString = lowerString.toUpperCase();
   const numericString = "1234567890";
-  const specialString = "!@#$%^&*()_-+=`~";
-
+  const specialString = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
+  //string used to build the end string used for creating the password
   let charPool = "";
 
+  //get user prefferences for password structure
+  let [length, lower, upper, numeric, special] = getInput();
+
+  //build charPool string based on user selected options
   if(lower) {charPool = charPool.concat(lowerString);}
   if(upper) {charPool = charPool.concat(upperString);}
   if(numeric) {charPool = charPool.concat(numericString);}
   if(special) {charPool = charPool.concat(specialString);}
 
+  //initialize password length and password string
   let possibleSelect = charPool.length;
   let password = "";
 
+  //generate password
   for(var i = 0; i < length; i++) {
     var random = cryptoRandom();
     password = password.concat(charPool[Math.floor(random * possibleSelect)]);
@@ -87,13 +54,15 @@ function cryptoRandom() {
   return arr[0]/(0xffffffff + 0.9999999999);
 }
 
+//gets user input and validates it
+//returns array[5] with [number length, bool lower, bool upper, bool numeric, bool special]
+function getInput() {
 
-function validate() {
   //used to verify input has been validated
   let isValidated = false;
 
   //get password length from user between 8 and 128 characters
-  let length = 8;
+  let length = 0;
 
   while(!isValidated) {
     length = parseInt(prompt("Please choose a password length between 8 and 128 characters", "8"));
@@ -101,6 +70,8 @@ function validate() {
   }
   
   isValidated = false;
+
+  //get user options for lower/uppercase, numeric/special characters
   let lower,upper,numeric,special;
 
   while(!isValidated) {
